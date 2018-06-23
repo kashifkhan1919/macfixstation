c0c3d8404061203bb98d38e889a7628e335b6290
Kashif Khan <kashif_khan1919@yahoo.com>
2018-06-23 18:04:38 +0530

    php-form-update

---
+++ freecontactformsettings.php
@@ -0,0 +1,11 @@
+<?php
+
+$email_to = "kashif_khan1919@yahoo.com"; // your email address
+$email_subject = "Contact form submission From MacfixStation New LP"; // email subject line
+$thankyou = "index.html"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+
+?>
